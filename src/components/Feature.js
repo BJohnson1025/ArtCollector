cfb6c32de4aa1e97f21b32358388c700ca5784a5
Brandon Johnson <developmentbybjohnson@gmail.com>
2023-06-04 13:53:01 -0500

    Edit all required files

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,45 +1,73 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 // Don't touch this import
-import { fetchQueryResultsFromTermAndValue } from '../api';
+import { fetchQueryResultsFromTermAndValue } from "../api";
 
 /**
  * We need a new component called Searchable which:
- * 
+ *
  * Has a template like this:
- * 
+ *
  * <span className="content">
  *  <a href="#" onClick={async (event) => {}}>SOME SEARCH TERM</a>
  * </span>
  *
  * You'll need to read searchTerm, searchValue, setIsLoading, and setSearchResults off of the props.
- * 
+ *
  * When someone clicks the anchor tag, you should:
- * 
+ *
  * - preventDefault on the event
  * - call setIsLoading, set it to true
- * 
+ *
  * Then start a try/catch/finally block:
- * 
+ *
  * try:
  *  - await the result of fetchQueryResultsFromTermAndValue, passing in searchTerm and searchValue
  *  - send the result to setSearchResults (which will update the Preview component)
- * catch: 
+ * catch:
  *  - console.error the error
  * finally:
  *  - call setIsLoading, set it to false
  */
-const Searchable = (props) => {
-  
-}
+const Searchable = ({
+  searchTerm,
+  searchValue,
+  setIsLoading,
+  setSearchResults,
+}) => {
+  return (
+    <span className="content">
+      <a
+        href="#"
+        onClick={async (event) => {
+          event.preventDefault();
+          setIsLoading(true);
+          try {
+            const response = await fetchQueryResultsFromTermAndValue(
+              searchTerm,
+              searchValue.replaceAll(" ", "-")
+            );
+            setSearchResults(response);
+          } catch (err) {
+            console.error(err);
+          } finally {
+            setIsLoading(false);
+          }
+        }}
+      >
+        {searchValue}
+      </a>
+    </span>
+  );
+};
 
 /**
  * We need a new component called Feature which looks like this when no featuredResult is passed in as a prop:
- * 
+ *
  * <main id="feature"></main>
- * 
+ *
  * And like this when one is:
- * 
+ *
  * <main id="feature">
  *   <div className="object-feature">
  *     <header>
@@ -57,18 +85,118 @@ const Searchable = (props) => {
  *     </section>
  *   </div>
  * </main>
- * 
- * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style, 
+ *
+ * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style,
  * technique, medium, dimensions, people, department, division, contact, creditline
- * 
+ *
  * The <Searchable /> ones are: culture, technique, medium (first toLowerCase it), and person.displayname (one for each PEOPLE)
- * 
+ *
  * NOTE: people and images are likely to be arrays, and will need to be mapped over if they exist
- * 
+ *
  * This component should be exported as default.
  */
-const Feature = (props) => {
-
-}
+const Feature = ({ featuredResult, setIsLoading, setSearchResults }) => {
+  const searchFacts = ["culture", "technique", "medium"];
+  const otherFacts = [
+    "description",
+    "dated",
+    "style",
+    "dimensions",
+    "department",
+    "division",
+    "contact",
+    "creditline",
+  ];
+  return (
+    <>
+      {!featuredResult ? (
+        <main id="feature"></main>
+      ) : (
+        <main id="feature">
+          <div className="object-feature">
+            <header>
+              <h3>{featuredResult.title}</h3>
+              <h4>{featuredResult.dated}</h4>
+            </header>
+            <section className="facts">
+              {searchFacts.map((el, ind) => {
+                return (
+                  <React.Fragment key={ind + el}>
+                    {featuredResult[el] ? (
+                      <>
+                        <span
+                          className="title"
+                          style={{ textTransform: "capitalize" }}
+                        >
+                          {el}
+                        </span>
+                        <Searchable
+                          searchValue={featuredResult[el]}
+                          searchTerm={el}
+                          setIsLoading={setIsLoading}
+                          setSearchResults={setSearchResults}
+                        />
+                      </>
+                    ) : null}
+                  </React.Fragment>
+                );
+              })}
+              {featuredResult.people
+                ? featuredResult.people.map((el, ind) => {
+                    return (
+                      <React.Fragment key={ind + el.displayname}>
+                        <span
+                          className="title"
+                          style={{ textTransform: "capitalize" }}
+                        >
+                          person
+                        </span>
+                        <Searchable
+                          searchValue={el.displayname}
+                          searchTerm={"displayname"}
+                          setIsLoading={setIsLoading}
+                          setSearchResults={setSearchResults}
+                        />
+                      </React.Fragment>
+                    );
+                  })
+                : null}
+              {otherFacts.map((el, ind) => {
+                return (
+                  <React.Fragment key={ind + el}>
+                    {featuredResult[el] ? (
+                      <>
+                        <span
+                          className="title"
+                          style={{ textTransform: "capitalize" }}
+                        >
+                          {el}
+                        </span>
+                        <span className="content">{featuredResult[el]}</span>
+                      </>
+                    ) : null}
+                  </React.Fragment>
+                );
+              })}
+            </section>
+            <section className="photos">
+              {featuredResult.images
+                ? featuredResult.images.map((el, ind) => {
+                    return (
+                      <img
+                        key={`${ind}-${el.imageid}`}
+                        src={el.baseimageurl}
+                        alt={el.alttext}
+                      />
+                    );
+                  })
+                : null}
+            </section>
+          </div>
+        </main>
+      )}
+    </>
+  );
+};
 
 export default Feature;
